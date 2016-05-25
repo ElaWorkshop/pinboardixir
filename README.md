60d4491b9ad41c3b1a6e028bee75f900ecfbd2b4
AquarHEAD Lou <aquarhead@gmail.com>
2016-05-26 04:18:27 +0800

    Publish 0.0.1 on hex.pm

--- README.md
+++ README.md
@@ -1,20 +1,3 @@
 # Pinboardixir
 
-**TODO: Add description**
-
-## Installation
-
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
-
-  1. Add pinboardixir to your list of dependencies in `mix.exs`:
-
-        def deps do
-          [{:pinboardixir, "~> 0.0.1"}]
-        end
-
-  2. Ensure pinboardixir is started before your application:
-
-        def application do
-          [applications: [:pinboardixir]]
-        end
-
+A Pinboard client in Elixir.
--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Pinboardixir.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -14,8 +16,23 @@ defmodule Pinboardixir.Mixfile do
     [applications: [:logger, :httpoison]]
   end
 
+  defp description do
+    "A Pinboard client in Elixir."
+  end
+
   defp deps do
     [{:httpoison, "~> 0.8"},
      {:poison, "~> 2.0"}]
+  end
+
+  defp package do
+    [
+      name: :pinboardixir,
+      maintainers: ["Lou Xun"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/ElaWorkshop/pinboardixir"
+      }
+    ]
   end
 end
