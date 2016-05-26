807cb8df3be03391fb139e1aebeff016555dfe82
AquarHEAD Lou <aquarhead@gmail.com>
2016-05-26 15:47:58 +0800

    Release 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pinboardixir.Mixfile do
 
   def project do
     [app: :pinboardixir,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
