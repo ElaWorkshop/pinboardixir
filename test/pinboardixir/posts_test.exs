1b61391d8fa5dd2bd3922400e482b397be49664f
AquarHEAD Lou <aquarhead@gmail.com>
2016-05-27 17:45:20 +0800

    Add test for Posts.get/1

--- test/pinboardixir/posts_test.exs
+++ test/pinboardixir/posts_test.exs
@@ -12,11 +12,31 @@ defmodule Pinboardixir.PostsTest do
       assert conn.request_path == "/posts/all"
       assert conn.method == "GET"
 
-      Plug.Conn.resp(conn, 200, ~s<[{"href":"https:\/\/www.inverse.com\/article\/10674-here-s-how-inverse-tests-external-apis-in-elixir-with-bypass","description":"Here's How Inverse Tests External APIs in Elixir with Bypass | Inverse","extended":"","meta":"84e86a26498c391f66663d0974f6001e","hash":"f9153afe872f421073cda4219d0429d0","time":"2016-05-26T11:17:28Z","shared":"yes","toread":"no","tags":"Elixir Test Bypass Mock"},{"href":"http:\/\/nsomar.com\/elixir-enumerated-types\/","description":"Enumerated types in Elixir","extended":"","meta":"aace15c2b83cd4a7bedf12bd430677ff","hash":"165f8a7353a76c7c3a3a1458d84b3420","time":"2016-05-26T09:12:09Z","shared":"yes","toread":"no","tags":"Elixir"}]>)
+      Plug.Conn.resp(conn, 200, ~s"""
+      [{"href":"https:\/\/www.inverse.com\/article\/10674-here-s-how-inverse-tests-external-apis-in-elixir-with-bypass","description":"Here's How Inverse Tests External APIs in Elixir with Bypass | Inverse","extended":"","meta":"84e86a26498c391f66663d0974f6001e","hash":"f9153afe872f421073cda4219d0429d0","time":"2016-05-26T11:17:28Z","shared":"yes","toread":"no","tags":"Elixir Test Bypass Mock"},{"href":"http:\/\/nsomar.com\/elixir-enumerated-types\/","description":"Enumerated types in Elixir","extended":"","meta":"aace15c2b83cd4a7bedf12bd430677ff","hash":"165f8a7353a76c7c3a3a1458d84b3420","time":"2016-05-26T09:12:09Z","shared":"yes","toread":"no","tags":"Elixir"}]
+      """)
     end
 
     posts = Pinboardixir.Posts.all()
 
     assert Enum.count(posts) == 2
+  end
+
+  test "`get/1` should return a list of `Pinboardixir.Post` for a given date", %{bypass: bypass} do
+    Bypass.expect bypass, fn conn ->
+      assert conn.request_path == "/posts/get"
+      assert conn.method == "GET"
+      assert String.contains?(conn.query_string, "dt=2016-05-26")
+
+      Plug.Conn.resp(conn, 200, ~s"""
+      {"date":"2016-05-26T09:12:09Z","user":"aquarhead","posts":[{"href":"http:\/\/nsomar.com\/elixir-enumerated-types\/","description":"Enumerated types in Elixir","extended":"","meta":"aace15c2b83cd4a7bedf12bd430677ff","hash":"165f8a7353a76c7c3a3a1458d84b3420","time":"2016-05-26T09:12:09Z","shared":"yes","toread":"no","tags":"Elixir"},{"href":"https:\/\/www.inverse.com\/article\/10674-here-s-how-inverse-tests-external-apis-in-elixir-with-bypass","description":"Here's How Inverse Tests External APIs in Elixir with Bypass | Inverse","extended":"","meta":"84e86a26498c391f66663d0974f6001e","hash":"f9153afe872f421073cda4219d0429d0","time":"2016-05-26T11:17:28Z","shared":"yes","toread":"no","tags":"Elixir Test Bypass Mock"}]}
+      """)
+    end
+
+    posts = Pinboardixir.Posts.get([dt: "2016-05-26"])
+
+    assert Enum.count(posts) == 2
+
+    #TODO: Add date check
   end
 end
--- test/pinboardixir_test.exs
+++ test/pinboardixir_test.exs
@@ -1,8 +1,3 @@
 defmodule PinboardixirTest do
   use ExUnit.Case
-  doctest Pinboardixir
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
 end
