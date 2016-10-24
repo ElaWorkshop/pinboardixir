de12630c54e5a2f8d35b7c7b70567a55a8983fdf
AquarHEAD Lou <aquarhead@ela.build>
2016-10-24 20:48:04 +0000

    Update Poison

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Pinboardixir.Mixfile do
 
   defp deps do
     [{:httpoison, "~> 0.8"},
-     {:poison, "~> 2.0"},
+     {:poison, "~> 3.0"},
      {:ex_doc, "~> 0.14", only: :dev},
      {:earmark, "~> 1.0", only: :dev},
      {:credo, "~> 0.4.0-beta2", only: :dev},
